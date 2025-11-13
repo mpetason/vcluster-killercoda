1316132583c896fdc62263542d90e29271ed853b
Mike Petersen <mike.petersen@loft.sh>
2025-11-13 12:32:12 -0800

    added foreground welcome

--- vCluster-101/setup.sh
+++ vCluster-101/setup.sh
@@ -1,9 +1,1 @@
 apt-get install kubectx -y
-
-touch welcome.sh
-
-echo printf '\n\033[1;38;5;45m╔══════════════════════════════════════════════════════╗\033[0m\n' >> welcome.sh
-echo printf '\033[1;38;5;45m║\033[0m \033[1;97mWelcome to v101 - vCluster Basics!\033[0m          \033[1;38;5;45m║\033[0m\n' >> welcome.sh
-printf '\033[1;38;5;45m╚══════════════════════════════════════════════════════╝\033[0m\n\n' >> welcome.sh
-
-sudo sh ./welcome.sh
