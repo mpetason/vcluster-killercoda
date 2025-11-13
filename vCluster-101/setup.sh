189d7f9fad0dffda064f110f79eec02efbdc895d
Mike Petersen <mike.petersen@loft.sh>
2025-11-13 12:28:10 -0800

    add welcome

--- vCluster-101/setup.sh
+++ vCluster-101/setup.sh
@@ -2,9 +2,8 @@ apt-get install kubectx -y
 
 touch welcome.sh
 
-echo printf '\n\033[1;38;5;45m╔══════════════════════════════════════════════════════╗\033[0m\n'
-printf '\033[1;38;5;45m║\033[0m \033[1;97mWelcome to v101 - vCluster Basics!\033[0m          \033[1;38;5;45m║\033[0m\n'
-printf '\033[1;38;5;45m╚══════════════════════════════════════════════════════╝\033[0m\n\n'' >> welcome.sh
+echo printf '\n\033[1;38;5;45m╔══════════════════════════════════════════════════════╗\033[0m\n' >> welcome.sh
+echo printf '\033[1;38;5;45m║\033[0m \033[1;97mWelcome to v101 - vCluster Basics!\033[0m          \033[1;38;5;45m║\033[0m\n' >> welcome.sh
+printf '\033[1;38;5;45m╚══════════════════════════════════════════════════════╝\033[0m\n\n' >> welcome.sh
 
-chmod +x welcome.sh
-./welcome.sh
+sudo sh ./welcome.sh
